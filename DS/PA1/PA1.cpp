a144fc1e87d864fe7591af1928a748ae5e4dbed2
Castiel <hujy652@gmail.com>
2016-09-18 22:35:16 +0800

    reformat with clang-format

--- DS/PA1/PA1.cpp
+++ DS/PA1/PA1.cpp
@@ -2,63 +2,55 @@
 
 using namespace std;
 
-struct Card
-{
-	int Rank;
-	int Suit;
+struct Card {
+    int Rank;
+    int Suit;
 };
 
-Class Hand
-{
-	public:
-		Hand( char input[6][3] );
-		void CheckType();
-	private:
-		Card HandCard[5];
-		int Pair1[2];
-		int Pair2[2];
+class Hand {
+  public:
+    Hand(char input[6][3]);
+    void CheckType();
+
+  private:
+    Card HandCard[5];
+    int Pair1[2];
+    int Pair2[2];
 };
 
-int ConvertSuitToNumber( char suit )
-{
-	if ( suit == J )
-	{
-		return 11;
-	}
-	else if ( suit == Q )
-	{
-		return 12;
-	}
-	else if ( suit == K )
-	{
-		return 13;
-	}
-	else if ( suit == A )
-	{
-		return 14;
-	}
-	return ( suit - '0' );
+int ConvertSuitToNumber(char suit) {
+    if (suit == J) {
+	return 11;
+    } else if (suit == Q) {
+	return 12;
+    } else if (suit == K) {
+	return 13;
+    } else if (suit == A) {
+	return 14;
+    }
+    return (suit - '0');
 }
 
-
-Hand::Hand( char input[6][3] )
-{
-	for( int i = 0; i < 5; ++i )
-	{
-		HandCard[i].Rank = input[i][0] - '0';
-		HandCard[i].Suit = ConvertSuitToNumber( input[i][1] );
-	}
+Hand::Hand(char input[6][3]) {
+    for (int i = 0; i < 5; ++i) {
+	HandCard[i].Rank = input[i][0] - '0';
+	HandCard[i].Suit = ConvertSuitToNumber(input[i][1]);
+    }
 }
 
+Hand::~Hand(){};
 
-int main()
-{
-	char case[6][3];
-	for( int i = 0; i < 5, ++i )
-	{
-		cin >> case[i][1] >> case[i][2];
-	}
-	Hand Player1( case );
+int main() {
+    char case[6][3];
+    for (int i = 0; i < 5, ++i) {
+	cin >> case[i][0] >> case[i][1];
+    }
+    Hand Player1(case);
 
-	return 0;
+    for (int i = 0; i < 5, ++i) {
+	cin >> case[i][0] >> case[i][1];
+    }
+    Hand Player2(case);
+
+    return 0;
 }
