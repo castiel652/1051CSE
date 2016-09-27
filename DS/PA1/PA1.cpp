935de6ef7335284aba9b0c91473d6d7a948ec153
Castiel Yu <hujy652@gmail.com>
2016-09-27 20:35:04 +0800

    fuck you git

--- DS/PA1/PA1.cpp
+++ DS/PA1/PA1.cpp
@@ -2,127 +2,63 @@
 
 using namespace std;
 
-struct Card{
-	int Suit;
+struct Card
+{
 	int Rank;
+	int Suit;
 };
 
-struct Hand{
-	Card HandCard[5];
-	Card Pair1[5];
-	Card Pair2[5];
-	int type1;
-	int type2;
-	bool twopair;
-};
-
-int ConvertRank(char Rank)
+Class Hand
 {
-	if(Rank == 'J')
+	public:
+		Hand( char input[6][3] );
+		void CheckType();
+	private:
+		Card HandCard[5];
+		int Pair1[2];
+		int Pair2[2];
+};
+
+int ConvertSuitToNumber( char suit )
+{
+	if ( suit == J )
+	{
 		return 11;
-	else if(Rank == 'Q')
+	}
+	else if ( suit == Q )
+	{
 		return 12;
-	else if(Rank == 'K')
+	}
+	else if ( suit == K )
+	{
 		return 13;
-	else if(Rank == 'T')
-		return 10;
-	else if(Rank == 'A')
+	}
+	else if ( suit == A )
+	{
 		return 14;
-	else
-		return (Rank - '0');
-}
-
-int ConvertSuit(char Suit)
-{
-	if(Suit == 'C')
-		return 1;
-	else if(Suit == 'D')
-		return 2;
-	else if(Suit == 'H')
-		return 3;
-	else if(Suit == 'S')
-		return 4;
-}
-
-bool St(Card input[]) //Straight
-{
-	if(input[0].Rank == input[1].Rank + 1 && input[1].Rank == input[2].Rank + 1 && input[2].Rank == Card[3].Rank + 1 && Card[3].Rank == Card[4].Rank + 1)
-		return true;
-	return false;
-}
-
-bool FK(Card input[]) //Four of a Kind
-{
-	for(int i = 0; i < 2; i++)
-	{
-		for( int a = i + 1; a < 5; ++a)
-		{
-			if((a == 3 || a == 4) && input[i].Suit == input[a].Suit)
-				return true;
-			if(input[i].Suit != input[a].Suit)
-				break;
-		}
 	}
-	return false;
+	return ( suit - '0' );
 }
 
-bool Sf(Card input[]) //Straight Flush
-{
-	if(input[0].Suit == input[1].Suit == input[2].Suit == input[3].Suit == input[4].Suit)
-	{
-		if(St(input[6]))
-		return true;
-	}
-	return false;
-}
 
-void sort(&Card input[])
+Hand::Hand( char input[6][3] )
 {
-	Card temp;
-	for(int i = 0; i < 5; ++i)
+	for( int i = 0; i < 5; ++i )
 	{
-		for(int a = i + 1; a < 5; ++i)
-		{
-			if(input[i].Rank < input[a].Rank)
-			{
-				temp.Suit = input[i].Suit;
-				temp.Rank = input[i].Rank;
-				input[i].Suit = input[a].Suit;
-				input[i].Rank = input[a].Rank;
-				input[a].Suit = temp.Suit;
-				input[a].Rank = temp.Rank;
-			}
-		}
+		HandCard[i].Rank = input[i][0] - '0';
+		HandCard[i].Suit = ConvertSuitToNumber( input[i][1] );
 	}
 }
 
-bool (*type)(Card[]) = {, HC, Pa, TP, TK, St, Fl, FH, FK, Sf};
 
 int main()
 {
-	char input[21];
-	Hand Player1;
-	Hand Player2;
-
-	while(getline(cin, input))
+	char case[6][3];
+	for( int i = 0; i < 5, ++i )
 	{
-		for(int i = 0; i < 10; ++i)
-		{
-			if(i % 2 == 0)
-				Player1.HandCard[i].Suit = ConvertSuit(input[i]);
-			else
-				Player1.HandCard[i].Rank = ConvertRank(input[i]);
-		}
-		Sort(Player1.HandCard[6]);
-		for(int i = 11; i < 20; ++i)
-		{
-			if(i % 2 == 0)
-				Player2.HandCard[i].Suit = ConvertSuit(input[i]);
-			else
-				Player2.HandCard[i].Rank = ConvertRank(input[i]);
-		}
-		Sort(Player2.HandCard[6]);
+		cin >> case[i][1] >> case[i][2];
 	}
+	Hand Player1( case );
 
 	return 0;
 }
