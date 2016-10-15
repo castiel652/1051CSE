b12e93d94757f2471bc008ca8820d1de7a2fcf06
Castiel Yu <hujy652@gmail.com>
2016-10-15 23:28:10 +0800

    Finished UVA 11988

--- DS/PA5/BrokenKeyboard.cpp
+++ DS/PA5/BrokenKeyboard.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
-#include <string>
+#include <list>
 
 using namespace std;
 
-void main()
+int main()
 {
+	string input;
+	list<char> beiju;
+	list<char>::iterator j;
+	while(getline(cin, input,'\n')) {
+		j = beiju.begin();
+		for(int i = 0; i < input.size(); ++i) {
+			if(input[i] == '[') /* move to begin if encounter '[' */
+				j = beiju.begin();
+			else if(input[i] == ']') /* move to end if encounter ']' */
+				j = beiju.end();
+			else
+				beiju.insert(j,input[i]);
+		}
+		for(list<char>::iterator it = beiju.begin(); it != beiju.end(); ++it)
+			cout << *it;
+		cout << endl;
+		beiju.clear();
+	}
+	return 0;
 }
